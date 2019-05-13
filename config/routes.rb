58d300880487db48dfc67f2971cc9bdd8f952181
Patrick Kim <pkim050@gmail.com>
2019-05-13 22:46:45 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   resources :authors, only: [:show] do
     resources :posts, only: [:show, :index]
   end
-  end
 
   resources :posts, only: [:index, :show, :new, :create, :edit, :update]
 
